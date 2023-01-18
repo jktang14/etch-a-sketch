c0159958484fa3500900c7d685217f1bb9e4f4c6
jktang14 <97177453+jktang14@users.noreply.github.com>
2023-01-18 17:14:58 +0800

    Set color change when grid hovered over

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
     cells = document.createElement('div');
-    cells.classList.add('Grid-item');
+    cells.classList.add('grid-item');
     container.appendChild(cells);
 }
+
+// Gives node list of grid items
+item = document.querySelectorAll(".grid-item");
+
+function changeColor(e) {
+    this.style.backgroundColor = "rgb(218, 87, 87)";
+}
+
+item.forEach((element) => element.addEventListener('mouseover', changeColor));
+
 
