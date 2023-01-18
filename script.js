7f2c2e1281771fe44cc6590c7c9ac712a5ef0eee
jktang14 <97177453+jktang14@users.noreply.github.com>
2023-01-18 16:45:20 +0800

    Add class to each grid item

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
     cells = document.createElement('div');
-    container.appendChild(cells)
+    cells.classList.add('Grid-item');
+    container.appendChild(cells);
 }
+
